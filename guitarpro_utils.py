949d9241a2904868b98151cb777fb1aed60b78ec
Frank Cwitkowitz <cwitkowitz@gmail.com>
2023-02-04 22:37:26 -0500

    Laying foundation with guitarpro parsing scripts from guitar-transcription-with-inhib.

---
+++ guitarpro_utils.py
@@ -0,0 +1,393 @@
+# Author: Jonathan Driedger (Chordify)
+# Author: Frank Cwitkowitz <fcwitkow@ur.rochester.edu>
+
+from copy import deepcopy
+
+import numpy as np
+import librosa # TODO - can likely remove this dependency
+
+TICKS_PER_QUARTER_NOTE = 960
+NOTE_TYPE_ENUM_REST    = 'rest'
+NOTE_TYPE_ENUM_NORMAL  = 'normal'
+NOTE_TYPE_ENUM_TIE     = 'tie'
+NOTE_TYPE_ENUM_DEAD    = 'dead'
+
+
+def ticks_to_seconds(ticks, tempo):
+    """
+    Convert an amount of ticks to a concrete time.
+
+    Parameters
+    ----------
+    ticks : int or float
+      Amount of ticks
+    tempo : int or float
+      Number of beats per minute
+
+    Returns
+    ----------
+    time : float
+      Time in seconds corresponding to number of ticks
+    """
+
+    # Number of seconds per beat times number of quarter beats
+    time = (60 / tempo) * ticks / TICKS_PER_QUARTER_NOTE
+
+    return time
+
+
+class Note(object):
+    """
+    Simple class representing a guitar note for use during GuitarPro file processing.
+    """
+
+    def __init__(self, fret, onset, duration, string=None):
+        """
+        Initialize a guitar note.
+
+        Parameters
+        ----------
+        fret : int
+          Fret the note was played on
+        onset : float
+          Time of the beginning of the note in seconds
+        duration : float
+          Amount of time after the onset where the note is still active
+        string : int (Optional)
+          Numerical indicator for the string the note was played on
+        """
+
+        self.fret = fret
+        self.onset = onset
+        self.duration = duration
+        self.string = string
+
+    def extend_note(self, duration):
+        """
+        Extend the note by a specified amount of time.
+
+        Parameters
+        ----------
+        duration : float
+          Amount of time to extend the note
+        """
+
+        self.duration = duration
+
+
+class NoteTracker(object):
+    """
+    Simple class to keep track of state while tracking notes in a GuitarPro file.
+    """
+
+    def __init__(self, default_tempo, tuning=None):
+        """
+        Initialize the state of the tracker.
+
+        Parameters
+        ----------
+        default_tempo : int or float
+          Underlying tempo of the track
+        tuning : list or ndarray
+            MIDI pitch of each open-string
+        """
+
+        # Keep track of both the underlying and current tempo
+        self.default_tempo = default_tempo
+        self.current_tempo = default_tempo
+
+        # Keep track of the tuning and string names
+        self.string_keys = librosa.midi_to_note(tuning)
+
+        # Dictionary to hold all notes
+        self.stacked_gpro_notes = dict()
+        # Loop though the tuning from lowest to highest note
+        for pitch in sorted(tuning):
+            # Determine the corresponding key for the string
+            key = librosa.midi_to_note(pitch)
+            # Add an empty list as an entry for the string
+            self.stacked_gpro_notes[key] = list()
+
+    def set_current_tempo(self, tempo=None):
+        """
+        Update the currently tracked tempo.
+
+        Parameters
+        ----------
+        tempo : int or float (Optional)
+          New tempo
+        """
+
+        if tempo is None:
+            # Reset the current tempo to the default
+            self.current_tempo = self.default_tempo
+        else:
+            # Update the current tempo
+            self.current_tempo = tempo
+
+    def get_current_tempo(self):
+        """
+        Obtain the currently tracked tempo.
+
+        Returns
+        ----------
+        tempo : int or float (Optional)
+          Current tracked tempo
+        """
+
+        tempo = self.current_tempo
+
+        return tempo
+
+    def track_note(self, gpro_note, onset, duration):
+        """
+        Update the currently tracked tempo.
+
+        Parameters
+        ----------
+        gpro_note : PyGuitarPro Note object
+          GuitarPro note information
+        onset : float
+          Time the note begins in seconds
+        duration : float
+          Amount of time the note is active
+        """
+
+        # TODO - see https://github.com/Perlence/PyGuitarPro/blob/master/guitarpro/models.py
+        #      - for potential information we should track in JAMS files
+        #      - ... or should we maintain score as tokens (https://github.com/dada-bots/dadaGP)?
+
+        # Extraction all relevant note information
+        string_idx, fret, type = gpro_note.string - 1, gpro_note.value, gpro_note.type.name
+
+        # TODO - determine how to deal with these (and other) NoteEffects
+        if gpro_note.effect.ghostNote:
+            pass
+        if gpro_note.effect.letRing:
+            pass
+        if gpro_note.effect.staccato:
+            pass
+
+        # Scale the duration by the duration percentage
+        duration *= gpro_note.durationPercent
+
+        # Create a note object to keep track of the GuitarPro note
+        note = Note(fret, onset, duration, string_idx)
+
+        # Get the key corresponding to the string index
+        key = self.string_keys[string_idx]
+
+        if type == NOTE_TYPE_ENUM_NORMAL:
+            # Add the new note to the dictionary under the respective string
+            self.stacked_gpro_notes[key].append(note)
+        elif type == NOTE_TYPE_ENUM_TIE:
+            # Obtain the last note that occurred on the string
+            last_gpro_note = self.stacked_gpro_notes[key][-1] \
+                             if len(self.stacked_gpro_notes[key]) else None
+            # Determine if the last note should be extended
+            if last_gpro_note is not None:
+                # Determine how much to extend the note
+                new_duration = onset - last_gpro_note.onset + duration
+                # Extend the previous note by the current beat's duration
+                last_gpro_note.extend_note(new_duration)
+        else:
+            pass
+
+    """
+    def get_stacked_notes(self):
+        "/""
+        Obtain the tracked GuitarPro notes as stacked notes.
+
+        Returns
+        ----------
+        stacked_notes : dict
+          Dictionary containing (slice -> (pitches, intervals)) pairs
+        "/""
+
+        # Initialize a new dictionary to hold the notes
+        stacked_notes = dict()
+
+        # Loop through all strings
+        for key in self.stacked_gpro_notes.keys():
+            # Initialize empty arrays to hold note contents
+            pitches, intervals = np.empty(0), np.empty((0, 2))
+
+            # Loop through all the GuitarPro notes for the string
+            for note in self.stacked_gpro_notes[key]:
+                # Add the absolute pitch of the note
+                pitches = np.append(pitches, librosa.note_to_midi(key) + note.fret)
+                # Add the onset and offset of the note
+                intervals = np.append(intervals, [[note.onset, note.onset + note.duration]], axis=0)
+
+            # Populate the dictionary with the notes for the string
+            stacked_notes.update(tools.notes_to_stacked_notes(pitches, intervals, key))
+
+        return stacked_notes
+    """
+
+
+def validate_gpro_track(gpro_track, tuning=None):
+    """
+    Helper function to determine which GuitarPro tracks are valid for our purposes.
+
+    Parameters
+    ----------
+    gpro_track : Track object (PyGuitarPro)
+      GuitarPro track to validate
+    tuning : list or ndarray
+        MIDI pitch of each open-string
+
+    Returns
+    ----------
+    valid : bool
+      Whether the GuitarPro track is considered valid
+    """
+
+    """
+    if tuning is None:
+        # Default the guitar tuning
+        tuning = librosa.note_to_midi(tools.DEFAULT_GUITAR_TUNING)
+    """
+
+    # Determine if this is a percussive track
+    percussive = gpro_track.isPercussionTrack
+
+    """
+    # Determine if this is a valid guitar track
+    guitar = (24 <= gpro_track.channel.instrument <= 31)
+
+    # Determine if the track has the expected number of strings in the expected tuning
+    expected_strings = set(tuning) == set([s.value for s in gpro_track.strings])
+    """
+
+    # Determine if the track is valid
+    valid = not percussive# and guitar and expected_strings
+
+    return valid
+
+
+def extract_stacked_notes_gpro_track(gpro_track, default_tempo):
+    """
+    Extract MIDI notes spread across strings within a GuitarPro track into a dictionary.
+
+    Parameters
+    ----------
+    gpro_track : Track object (PyGuitarPro)
+      GuitarPro track data
+    default_tempo : int
+      Track tempo for inferring note onset and duration
+
+    Returns
+    ----------
+    stacked_notes : dict
+      Dictionary containing (slice -> (pitches, intervals)) pairs
+    """
+
+    # Make a copy of the track, so that it can be modified without consequence
+    gpro_track = deepcopy(gpro_track)
+
+    # Obtain the tuning of the strings of the track
+    tuning = [string.value for string in gpro_track.strings]
+    # Initialize a tracker to keep track of GuitarPro notes
+    note_tracker = NoteTracker(default_tempo, tuning)
+
+    # Keep track of the amount of time processed so far
+    current_time = None
+
+    # Determine how many measures are in the track
+    total_num_measures = len(gpro_track.measures)
+
+    # Keep track of the current measure
+    current_measure = 0
+    # Keep track of the last measure which opened a repeat
+    repeat_measure = 0
+    # Keep track of the measure after the most recently encountered repeat close
+    next_jump = None
+
+    # Initialize a counter to keep track of how many times a repeat was obeyed
+    repeat_count = 0
+
+    # Loop through the track's measures
+    while current_measure < total_num_measures:
+        # Process the current measure
+        measure = gpro_track.measures[current_measure]
+
+        if measure.header.repeatAlternative != 0:
+            # The 'repeatAlternative' attribute seems to be encoded as binary a binary vector,
+            # where the integers k in the measure header represent a 1 in the kth digit
+            alt_repeat_num = np.sum([2 ** k for k in range(repeat_count)])
+            # Check if it is time to jump past the repeat close
+            if alt_repeat_num >= measure.header.repeatAlternative:
+                # Reset the external repeat counter
+                repeat_count = 0
+                # Indicate that this measure should always cause a jump from now on
+                measure.header.repeatAlternative = -1
+                # Jump past the repeat
+                current_measure = next_jump
+                continue
+
+        if measure.isRepeatOpen:
+            # Jump back to this measure at the next repeat close
+            repeat_measure = current_measure
+
+        # Keep track of the amount of time processed within the measure
+        measure_ticks = [0] * len(measure.voices)
+        measure_time = [0] * len(measure.voices)
+
+        # Loop through voices within the measure
+        for v, voice in enumerate(measure.voices):
+            # Loop through the beat divisions of the measure
+            for beat in voice.beats:
+                if current_time is None:
+                    # Set the current time to the start of the measure
+                    current_time = ticks_to_seconds(measure.start, note_tracker.get_current_tempo())
+
+                # Check if there are any tempo changes
+                if beat.effect.mixTableChange is not None:
+                    if beat.effect.mixTableChange.tempo is not None:
+                        # Extract the updated tempo
+                        new_tempo = beat.effect.mixTableChange.tempo.value
+                        # Update the tempo of the note tracker
+                        note_tracker.set_current_tempo(new_tempo)
+
+                # Convert the note duration from ticks to seconds
+                duration_seconds = ticks_to_seconds(beat.duration.time, note_tracker.get_current_tempo())
+
+                # Loop through the notes in the beat division
+                for note in beat.notes:
+                    # Add the note to the tracker
+                    note_tracker.track_note(note, current_time + measure_time[v], duration_seconds)
+
+                # Accumulate the time of the beat
+                measure_ticks[v] += beat.duration.time
+                measure_time[v] += duration_seconds
+
+        # Add the measure time to the current accumulated time
+        current_time += measure_time[0]
+        # Check if all ticks were counted
+        if measure_ticks[0] != measure.length:
+            # Compute the number of ticks missing
+            remaining_ticks = measure.length - measure_ticks[0]
+            # Add the time for the missing ticks
+            current_time += ticks_to_seconds(remaining_ticks, note_tracker.get_current_tempo())
+
+        if measure.repeatClose > 0:
+            # Set the (alternate repeat) jump to the next measure
+            next_jump = current_measure + 1
+            # Jump back to where the repeat begins
+            current_measure = repeat_measure
+            # Decrement the measure's repeat counter
+            measure.repeatClose -= 1
+            # Increment the external repeat counter
+            repeat_count += 1
+        else:
+            if measure.repeatClose == 0:
+                # Reset the external repeat counter
+                repeat_count = 0
+            # Increment the measure pointer
+            current_measure += 1
+
+    # Obtain the final tracked notes
+    #stacked_notes = note_tracker.get_stacked_notes()
+
+    #return stacked_notes
---
+++ process_guitarpro.py
@@ -0,0 +1,138 @@
+# Author: Jonathan Driedger (Chordify)
+# Author: Frank Cwitkowitz <fcwitkow@ur.rochester.edu>
+
+# My imports
+from guitarpro_utils import validate_gpro_track, extract_stacked_notes_gpro_track
+
+# Regular imports
+import numpy as np
+import guitarpro
+import os
+
+VALID_GP_EXTS = ['.gp3', '.gp4', '.gp5']
+INVALID_EXTS = ['.pygp', '.gp2tokens2gp']
+COPY_TAG = ' copy'
+
+# TODO - there are still some ways for duplicates to get in
+#      - e.g., very similar but not quite exact names
+#      - e.g., alternate version in another directory (ex: Jeopardy.gp3/4)
+#      - (n) tags when alternate version already exists (it doesn't always)
+#        - could this be true for the "copy" tag as well?
+
+
+def get_valid_files(base_dir, ignore_duplicates=True):
+    """
+    Walk through a base directory and keep track of all relevant GuitarPro files.
+
+    Parameters
+    ----------
+    base_dir : string
+      Path to the base directory to recursively search
+    ignore_duplicates : bool
+      Whether to remove exact and inferred duplicates
+
+    Returns
+    ----------
+    tracked_files : list of str
+      List of file names found
+    tracked_paths : list of str
+      List of paths corresponding to tracked files
+    """
+
+    # Keep track of valid GuitarPro files
+    tracked_paths, tracked_files = list(), list()
+
+    # Traverse through all paths within the base directory
+    for dir_path, dirs, files in os.walk(base_dir):
+        # Ignore directories with no files (only directories)
+        if not len(files):
+            continue
+
+        # Obtain a list of valid GuitarPro files within the current directory
+        valid_files = sorted([f for f in files
+                              if os.path.splitext(f)[-1] in VALID_GP_EXTS
+                              and INVALID_EXTS[0] not in f
+                              and INVALID_EXTS[1] not in f
+                              # Remove (exact) duplicates
+                              and not (f in tracked_files and ignore_duplicates)])
+
+        # Remove (inferred) duplicates within the directory
+        if ignore_duplicates:
+            # Obtain a list of copied files
+            copied_files = [f for f in valid_files if COPY_TAG in f]
+
+            # Loop through copies in the directory
+            for f in copied_files:
+                # Remove copies
+                valid_files.remove(f)
+
+            # Create a copy of the valid files to iterate through
+            valid_files_copy = valid_files.copy()
+
+            # Loop through the current valid files list
+            for i in range(0, len(valid_files) - 1):
+                # Obtain the current and next valid file
+                curr_file, next_file = valid_files_copy[i], valid_files_copy[i + 1]
+                # Check if the two files share the same name
+                if os.path.splitext(curr_file)[0] == os.path.splitext(next_file)[0]:
+                    # Remove the current file (should be earlier version)
+                    valid_files.remove(curr_file)
+
+        # Add valid files to tracked list
+        tracked_files += valid_files
+
+        # Update the tracked paths
+        tracked_paths += [dir_path] * len(valid_files)
+
+    return tracked_files, tracked_paths
+
+
+def guitarpro_to_jams(gpro_path, jams_dir):
+    """
+    Convert a GuitarPro file to a JAMS file specifying notes for each track.
+
+    TODO - add other stuff (beats, key, tempo, etc.) to the JAMS files
+
+    Parameters
+    ----------
+    gpro_path : string
+      Path to a preexisting GuitarPro file to convert
+    jams_dir : bool
+      Directory under which to place the JAMS files
+    """
+
+    # Make sure the JAMS directory exists
+    os.makedirs(jams_dir, exist_ok=True)
+
+    # Extract the GuitarPro data from the file
+    gpro_data = guitarpro.parse(gpro_path)
+
+    # Loop through the instrument tracks in the GuitarPro data
+    for t, gpro_track in enumerate(gpro_data.tracks):
+        # Make sure the GuitarPro file can be processed for symbolic datasets
+        if validate_gpro_track(gpro_track):
+            # Extract notes from the track, given the listed tempo
+            extract_stacked_notes_gpro_track(gpro_track, gpro_data.tempo)
+
+            # TODO - what exactly do we want here
+
+
+if __name__ == '__main__':
+    # Construct a path to the base directory
+    base_dir = '/home/rockstar/Desktop/Datasets/DadaGP'
+
+    # Search the specified path for GuitarPro files
+    tracked_files, tracked_paths = get_valid_files(base_dir)
+
+    # Construct a path to the JAMS directory
+    jams_dir = os.path.join(base_dir, 'jams')
+
+    # Loop through the tracked GuitarPro files
+    for k, gpro_file in enumerate(tracked_files):
+        print(f'Processing track \'{gpro_file}\'...')
+
+        # Construct a path to the GuitarPro file
+        gpro_path = os.path.join(tracked_paths[k], gpro_file)
+
+        # Perform the conversion
+        guitarpro_to_jams(gpro_path, jams_dir)
